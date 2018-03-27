d29b97145e38c8e24df19c86bef8d8ee699b343e
Denis.Verentsov <decsent@Yandex.ru>
2018-03-28 00:12:32 +0300

    Added di component, module, and graph.

---
+++ app/src/main/java/com/dvinc/notepad/di/components/AppComponent.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.di.components
+
+import com.dvinc.notepad.di.modules.AppModule
+import com.dvinc.notepad.ui.notepad.NotepadFragment
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = arrayOf(AppModule::class))
+interface AppComponent {
+    fun inject(notepadFragment: NotepadFragment)
+}
