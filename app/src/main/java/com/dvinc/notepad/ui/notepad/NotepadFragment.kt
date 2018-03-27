4e86f36193feb8e9c92a95dd34319df4048f70b1
Denis.Verentsov <decsent@Yandex.ru>
2018-03-27 17:58:52 +0300

    Added empty notepad fragment.

---
+++ app/src/main/java/com/dvinc/notepad/ui/notepad/NotepadFragment.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.ui.notepad
+
+import com.dvinc.notepad.R
+import com.dvinc.notepad.ui.base.BaseFragment
+
+class NotepadFragment : BaseFragment() {
+
+    override fun getFragmentLayoutId(): Int {
+        return R.layout.fragment_notepad
+    }
+}
