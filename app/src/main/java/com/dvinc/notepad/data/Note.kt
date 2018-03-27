b3426bf6dc579bb2894d9daf07154925c6b206e0
Denis.Verentsov <decsent@Yandex.ru>
2018-03-27 18:17:14 +0300

    Added notepadView interface and Notepad POJO.

---
+++ app/src/main/java/com/dvinc/notepad/data/Note.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.data
+
+data class Note(
+        val name: String,
+        val content: String,
+        val updateTime: Long)
