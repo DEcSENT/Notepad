deebedf690458b6e26fe2c31814a9456323a2f61
Denis.Verentsov <decsent@Yandex.ru>
2018-03-27 19:06:44 +0300

    Added recyclerView and note item.

--- app/src/main/java/com/dvinc/notepad/ui/notepad/NotepadFragment.kt
+++ app/src/main/java/com/dvinc/notepad/ui/notepad/NotepadFragment.kt
@@ -5,13 +5,33 @@
 
 package com.dvinc.notepad.ui.notepad
 
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.LinearLayout
+import butterknife.BindView
 import com.dvinc.notepad.R
+import com.dvinc.notepad.data.Note
 import com.dvinc.notepad.ui.base.BaseFragment
 
 class NotepadFragment : BaseFragment() {
 
+    @BindView(R.id.rv_notepad) lateinit var rvNotes: RecyclerView
+
     companion object {
         val TAG = "NotepadFragment"
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rvNotes.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+        val users = ArrayList<Note>()
+        users.add(Note("Note 1", "Well, Kotlin isn't looks so good...", System.currentTimeMillis()))
+        users.add(Note("Note 2", "But maybe sometimes i will learn it...", System.currentTimeMillis()))
+
+        val adapter = NotesAdapter(users)
+        rvNotes.adapter = adapter
     }
 
     override fun getFragmentLayoutId(): Int {
---
+++ app/src/main/java/com/dvinc/notepad/ui/notepad/NotesAdapter.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.ui.notepad
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.dvinc.notepad.R
+import com.dvinc.notepad.data.Note
+
+class NotesAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.name?.text = notes[position].name
+        holder?.content?.text = notes[position].content
+        holder?.updateTime?.text = notes[position].updateTime.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.item_note, parent, false)
+        return ViewHolder(v);
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.tv_note_name)
+        val content = itemView.findViewById<TextView>(R.id.tv_note_content)
+        val updateTime = itemView.findViewById<TextView>(R.id.tv_note_updated_time)
+    }
+
+    fun setNotes(notes : List<Note>) {
+        this.notes = notes
+    }
+}
