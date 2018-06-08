3f0b0d2434ed18a6f42caae05087d3c959d5d5e4
Denis Verentsov <decsent@yandex.ru>
2018-06-09 00:26:24 +0300

    Fixed NotesInteractor.

--- app/src/main/java/com/dvinc/notepad/domain/interactors/NotesInteractor.kt
+++ app/src/main/java/com/dvinc/notepad/domain/interactors/NotesInteractor.kt
@@ -7,21 +7,40 @@
 
 package com.dvinc.notepad.domain.interactors
 
+import com.dvinc.notepad.common.rxschedulers.RxSchedulers
+import com.dvinc.notepad.domain.mappers.NoteMapper
 import com.dvinc.notepad.domain.model.Note
 import com.dvinc.notepad.domain.model.NoteMarker
+import com.dvinc.notepad.domain.repositories.MarkersRepository
+import com.dvinc.notepad.domain.repositories.NotesRepository
 import io.reactivex.Completable
-import io.reactivex.Flowable
 import io.reactivex.Single
+import javax.inject.Inject
 
-interface NotesInteractor {
+/*
+ * This interactor is under construction.
+ * The main idea is obtain all needed data from different sources and prepare it for presenter and view.
+ */
+class NotesInteractor
+@Inject constructor(
+        private val notesRepository: NotesRepository,
+        private val markersRepository: MarkersRepository,
+        private val noteMapper: NoteMapper,
+        private val rxSchedulers: RxSchedulers
+){
 
-    fun getNotes(): Flowable<List<Note>>
+    fun getNoteById(id: Long): Single<Note> {
+        return notesRepository.getNoteById(id)
+                .compose(rxSchedulers.getIoToMainTransformerSingle())
+                .map { entity ->
+                    val markers = markersRepository.obtainMarkers()
+                    noteMapper.mapEntityToNote(entity, markers)
+                }
+    }
 
-    fun deleteNote(noteId: Int): Completable
-
-    fun getNoteById(id: Long): Single<Note>
-
-    fun getNoteMarkers(): Single<List<NoteMarker>>
+    fun getNoteMarkers(): Single<List<NoteMarker>> {
+        return markersRepository.getMarkers()
+    }
 
     fun addNoteInfo(
             noteId: Long?,
@@ -29,5 +48,15 @@ interface NotesInteractor {
             content: String,
             time: Long,
             markerId: Int
-    ): Completable
+    ): Completable {
+        return if (noteId != null && noteId != 0L) {
+            notesRepository.updateNote(
+                    noteMapper.createEntity(name, content, time, markerId, noteId))
+                    .compose(rxSchedulers.getIoToMainTransformerCompletable())
+        } else {
+            notesRepository.addNote(
+                    noteMapper.createEntity(name, content, time, markerId))
+                    .compose(rxSchedulers.getIoToMainTransformerCompletable())
+        }
+    }
 }
