adadd6348e57e0424b257f4d79030d29ecf1c333
Denis Verentsov <decsent@yandex.ru>
2018-08-05 13:41:09 +0300

    Added new classes for marker type.

--- app/src/main/java/com/dvinc/notepad/data/database/NotepadDatabase.kt
+++ app/src/main/java/com/dvinc/notepad/data/database/NotepadDatabase.kt
@@ -7,10 +7,13 @@ package com.dvinc.notepad.data.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.TypeConverters
+import com.dvinc.notepad.data.database.converter.MarkerTypeConverter
 import com.dvinc.notepad.data.database.entity.NoteEntity
 import com.dvinc.notepad.data.database.dao.NoteDao
 
 @Database(entities = arrayOf(NoteEntity::class), version = 1)
+@TypeConverters(MarkerTypeConverter::class)
 abstract class NotepadDatabase : RoomDatabase() {
     abstract fun notesDao(): NoteDao
 }
---
+++ app/src/main/java/com/dvinc/notepad/data/database/entity/MarkerTypeEntity.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.data.database.entity
+
+enum class MarkerTypeEntity {
+
+    NOTE,
+
+    CRITICAL,
+
+    TODO,
+
+    IDEA
+}
