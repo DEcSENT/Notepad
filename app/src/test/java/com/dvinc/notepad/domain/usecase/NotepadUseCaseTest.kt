2c1f1568e2d1d646255e3440cf939dac82c5b5d4
Denis Verentsov <decsent@yandex.ru>
2018-08-07 22:37:57 +0300

    Added tests for useCases.

---
+++ app/src/test/java/com/dvinc/notepad/domain/usecase/NotepadUseCaseTest.kt
@@ -0,0 +1,69 @@
+package com.dvinc.notepad.domain.usecase
+
+import com.dvinc.notepad.domain.TestThreadScheduler
+import com.dvinc.notepad.domain.model.Note
+import com.dvinc.notepad.domain.repository.NoteRepository
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import org.junit.Test
+
+import org.junit.Before
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class NotepadUseCaseTest {
+
+    @Mock
+    private lateinit var noteRepository: NoteRepository
+
+    @Mock
+    private lateinit var note: Note
+
+    private lateinit var notepadUseCase: NotepadUseCase
+
+    private lateinit var noteList: List<Note>
+
+    private val testScheduler = TestThreadScheduler()
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        notepadUseCase = NotepadUseCase(noteRepository, testScheduler)
+        noteList = listOf(note, note)
+
+        `when`(noteRepository.getNotes()).thenReturn(Flowable.just(noteList))
+        `when`(noteRepository.deleteNoteById(anyInt())).thenReturn(Completable.complete())
+    }
+
+    @Test
+    fun getNotes() {
+        notepadUseCase.getNotes()
+        verify(noteRepository).getNotes()
+    }
+
+    @Test
+    fun `check correct result from getNotes()`() {
+        notepadUseCase.getNotes()
+                .test()
+                .assertNoErrors()
+                .assertValue(noteList)
+    }
+
+    @Test
+    fun deleteNote() {
+        notepadUseCase.deleteNote(anyInt())
+        verify(noteRepository).deleteNoteById(anyInt())
+    }
+
+    @Test
+    fun `check correct result from deleteNote()`() {
+        notepadUseCase.deleteNote(anyInt())
+                .test()
+                .assertNoErrors()
+                .assertComplete()
+    }
+}
