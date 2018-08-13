580375257ce03fb8887c0c536b1f3ea2e4f08124
Denis Verentsov <decsent@yandex.ru>
2018-08-13 20:51:43 +0300

    Added resource provider.

---
+++ app/src/main/java/com/dvinc/notepad/common/resource/ResourceProviderImpl.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@Yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.notepad.common.resource
+
+import android.content.Context
+import javax.inject.Inject
+
+class ResourceProviderImpl @Inject constructor(
+        private val context: Context
+) : ResourceProvider {
+
+    override fun getString(resId: Int): String {
+        return context.getString(resId)
+    }
+}
