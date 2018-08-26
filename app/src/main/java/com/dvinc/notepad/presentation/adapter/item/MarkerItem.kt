ece8ec71d4eea57c8fcff8b01316335a857bf1ce
Denis Verentsov <decsent@yandex.ru>
2018-08-26 16:29:39 +0300

    Added marker item. Updated xml.

---
+++ app/src/main/java/com/dvinc/notepad/presentation/adapter/item/MarkerItem.kt
@@ -0,0 +1,28 @@
+package com.dvinc.notepad.presentation.adapter.item
+
+import android.graphics.PorterDuff
+import android.support.v4.content.ContextCompat
+import com.dvinc.notepad.R
+import com.dvinc.notepad.presentation.model.MarkerTypeUi
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.item_note_marker.ivMarkerIcon as markerIcon
+import kotlinx.android.synthetic.main.item_note_marker.tvMarkerText as markerText
+
+class MarkerItem(
+        private val marker: MarkerTypeUi
+) : Item() {
+
+    override fun getLayout() = R.layout.item_note_marker
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        with(viewHolder) {
+            itemView.context.let {
+                markerIcon.drawable.mutate().setColorFilter(
+                        ContextCompat.getColor(it, marker.markerColor),
+                        PorterDuff.Mode.MULTIPLY)
+                markerText.text = it.getString(marker.markerName)
+            }
+        }
+    }
+}
